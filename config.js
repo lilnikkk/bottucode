61fa25fc7dbcbba4da615bdf9682ba5c4d050e0c
NIk <127285319+lilnikkk@users.noreply.github.com>
2026-03-07 20:39:42 +0700

    Update Lavalink node configuration

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ const config = {
     nodeList: [
         {
             id: 'Node 1',
-            hostname: 'lavalinkv4.serenetia.com',
-            port: 80,
-            password: 'https://dsc.gg/ajidevserver'
+            hostname: 'sg1-nodelink.nyxbot.app',
+            port: 3000,
+            password: 'nyxbot.app/support'
         }
     ],
 
